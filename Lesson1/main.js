720a3ef17dc966a134219e0d657d7a4e807ecfd0
Michael160820 <belyayevm@gmail.com>
2023-07-22 16:21:45 +0300

    init commit

---
+++ Lesson1/main.js
@@ -0,0 +1,28 @@
+let a = 5;
+console.log(typeof a);
+console.log(5 > 8);
+let name = 'Max';
+let surName = 'Popov';
+let user = `${name} ${surName}`;
+console.log(user);
+console.log(5 - 3);
+console.log(14 % 3);
+console.log(5 ** 2);
+console.log(5 ** 2);
+console.log(5 ** 2);
+console.log(5 ** 2);//копировать строки контрл+D / command+d
+console.log(5 >= 3);
+console.log(5 >= 8);
+console.log(5 == 3);
+console.log(5 === 3);//use it
+console.log(5 != 3);
+console.log(5 !== 3);//use it
+console.log(true == 1);
+console.log(true === 1);
+console.log(150 + 150 + '');// string
+console.log('' + 150 + 150);//string 150150
+console.log(+'500');//number
+console.log('555' - 2);//number 553
+console.log(!true);//false
+let x = prompt();//всплывающее окно 
+console.log(x);
