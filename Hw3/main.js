8b96b7db9de331d12047f2e4684ac940b022eab7
Michael160820 <belyayevm@gmail.com>
2023-07-26 09:37:41 +0300

    hw 3 added

--- Hw3/main.js
+++ Hw3/main.js
@@ -1,162 +1,199 @@
-// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-function sRect(a, b) {
-    return a * b;
-}
+// // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// for (let i = 0; i < 10; i++) {
+//     document.write(`<div>SOME TEXT</div>`);
+// }
+//
+// // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+// for (let i = 0; i < 10; i++) {
+//     document.write(`<div>SOME TEXT with ${i} index</div>`);
+// }
+//
+// // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// let i = 0;
+// while (i < 20) {
+//     document.write(`<h1>SOME H1</h1>`);
+//     i++
+// }
+//
+// // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+// let y = 0;
+// while (y < 20) {
+//     document.write(`<h1>SOME H1 with ${y} index</h1>`);
+//     y++
+// }
+//
+// // - Використовуючи данні з масиву, за допомоги document.write та циклу
+// // побудувати структуру по шаблону
+// // ШАБЛОН:
+// //     <ul>
+// //         <li>ITEM OF ARRAY</li>
+// //         <!--
+// //             і тд інші об'єкти масиву
+// //              ...
+// //              ...
+// //              ...
+// //         -->
+// //     </ul>
+// // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// document.write(`<ul>`)
+// for (const item of listOfItems) {
+//     document.write(`<li>${item}</li>`)
+// }
+// document.write(`</ul>`)
+//
+// // -----------------------------------------------
+// //     Використовуючи данні з масиву, за допомоги document.write та циклу
+// // побудувати структуру по шаблону
+// // Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+// // ШАБЛОН
+// // <div class="product-card">
+// //     <h3 class="product-title">TITLE. Price - PRICE</h3>
+// // <img src="IMAGE" alt="" class="product-image">
+// // </div>
+// // Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+// for (const product of products) {
+//     document.write(`
+//        <div class="product-card">
+//           <h3 class="product-title">${product.title}. Price - ${product.price}</h3>
+//           <img src=${product.image} alt="" class="product-image" width="150px">
+//        </div>
+//     `)
+// }
 
-const sR = sRect(10, 5);
-console.log(sR);
+// // --------------------
+// //     є масив
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// console.log(users)
+// // за допомоги циклу вивести:
+// // - користувачів зі статусом true
+// for (const user of users) {
+//     if (user.status) {
+//         document.write(`<div>${user.name} - ${user.age} - ${user.status}</div>`);
+//     }
+// }
+// document.write('<hr>')
+// // - користувачів зі статусом false
+// for (const user of users) {
+//     if (!user.status) {
+//         document.write(`<div>${user.name} - ${user.age} - ${user.status}</div>`);
+//     }
+// }
+// document.write('<hr>')
+// // - користувачів які старші за 30 років
+// for (const user of users) {
+//     if (user.age > 30) {
+//         document.write(`<div>${user.name} - ${user.age} - ${user.status}</div>`);
+//     }
+// }
 
-// - створити функцію яка обчислює та повертає площу кола з радіусом r
-function sCir(r) {
-    return Math.PI * r ** 2;
-}
 
-const rCir = sCir(10);
-console.log(rCir);
+//
+let books = [
+    {
+        title: 'Deception Point',
+        pageCount: 560,
+        genre: ['Mystical detective'],
+        authors: ['Dan Brown', 'author 2'],
+    },
+    {
+        title: 'The Churchill Factor',
+        pageCount: 730,
+        genre: ['Political figures'],
+        authors: ['Boris Johnson']
+    },
+    {
+        title: 'Три товариші',
+        pageCount: 679,
+        genre: ['Classical prose'],
+        authors: ['Еріх Марія Ремарк']
+    },
+    {
+        title: 'Atomic HabitsAtomic HabitsAtomic HabitsAtomic Habits',
+        pageCount: 299,
+        genre: ['Self development', 'Motivation'],
+        authors: ['author 1', 'author 2'],
+    },
+    {
+        title: 'The Fine Art Of Small Talk',
+        pageCount: 74,
+        genre: ['Social communications'],
+        authors: ['author 1', 'author 2', 'author 3', 'author 4'],
+    },
+    {
+        title: 'The Book Thief',
+        pageCount: 412,
+        genre: ['Modern prose'],
+        authors: ['author 1', 'author 2'],
+    }
 
-// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-function sCil(r, h) {
-    return Math.PI * 2 * r * h;
-}
+]
+// // - знайти книжку/ки з найдовшою назвою
+// let result = books[0];
+// for (const book of books) {
+//     if (book.title.length > result.title.length) {
+//         result = book;
+//     }
+// }
+// console.log(result);
+// - знайти книжку/ки з найдовшою назвою
+// let result = books[0];
+// for (let i = 0; i < books.length; i++) {
+//     if (books[i].title.length > result.title.length) {
+//         result = books[i];
+//     }
+// }
+// console.log(result);
 
-const rCil = sCil(10, 5);
-console.log(rCil);
+// - знайти книжку/ки які писав 1 автор
+// let result = [];
+// for (const book of books) {
+//     if (book.authors.length === 1) {
+//         result[result.length] = book;
+//     }
+// }
+// console.log(result);
 
-// - створити функцію яка приймає масив та виводить кожен його елемент
-const array = [23, 444, 'sdfsdfdsf', true];
-
-function arrPrinter(arr) {
-    for (const item of arr) {
-        console.log(`item: ${item}`)
+// - знайти книжку які писав 1 автор
+let result;
+for (const book of books) {
+    if (book.authors.length === 1) {
+        result = book;
+        break;
     }
 }
-
-arrPrinter(array);
-
-// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
-function pCreator(text, esad) {
-    document.write(`<${esad}>${text}</${esad}>`);
-}
-
-pCreator('HELLO!!', 'h1');
-
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function ulCreator1(text) {
-    document.write(`
-        <ul>
-            <li>${text}</li>
-            <li>${text}</li>
-            <li>${text}</li>
-        </ul>
-    `);
-}
-
-ulCreator1('HELLO!!');
-
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function ulCreator2(text, count) {
-    document.write(`<ul>`);
-    for (let i = 0; i < count; i++) {
-        document.write(`<li>${text}</li>`)
-    }
-    document.write(`</ul>`);
-}
-
-ulCreator2('HELLO!!', 2);
-
-// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-const array2 = [23, 444, 'sdfsdfdsf', true, 'ddd', 'dddd'];
-
-function ulCreator3(arr) {
-    document.write(`<ul>`);
-    for (const item of arr) {
-        document.write(`<li>${item}</li>`)
-    }
-    document.write(`</ul>`);
-}
-
-ulCreator3(array2);
-
-// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-const users = [
-    {id: 1, name: 'vasya', age: 31},
-    {id: 2, name: 'petya', age: 30},
-    {id: 3, name: 'kolya', age: 29},
-    {id: 4, name: 'olya', age: 28},
-    {id: 5, name: 'max', age: 30},
-    {id: 6, name: 'anya', age: 31},
-    {id: 7, name: 'oleg', age: 28},
-    {id: 8, name: 'andrey', age: 29},
-    {id: 9, name: 'masha', age: 30},
-    {id: 10, name: 'olya', age: 31},
-    {id: 11, name: 'max', age: 31}
-];
-
-function usersPrinter(arr) {
-    for (const user of arr) {
-        document.write(`<div>id: ${user.id} - name: ${user.name} - age: ${user.age}</div>`);
-    }
-}
-
-usersPrinter(users);
-
-// - створити функцію яка повертає найменьше число з масиву
-const arrayOfNumbers = [234, -23, 55, 0, -99, 3455];
-
-function arrayMin(arr) {
-    let result = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (result > arr[i]) {
-            result = arr[i];
-        }
-    }
-    return result;
-}
-
-console.log(arrayMin(arrayOfNumbers));
-
-// - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
-function sum(arr) {
-    let result = 0;
-    for (const number of arr) {
-        result += number;
-    }
-    return result;
-}
-
-console.log(sum([1, 2, 10]));
-
-// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
-// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-function swap1(arr, index1, index2) {
-    const value1 = arr[index1];
-    const value2 = arr[index2];
-    arr[index1] = value2;
-    arr[index2] = value1;
-    return arr;
-}
-
-console.log(swap1([11, 22, 33, 44], 0, 1));
-
-function swap2(arr, index1, index2) {
-    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
-    return arr;
-}
-console.log(swap2([11, 22, 33, 44], 0, 1));
-
-// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
-// Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
-function exchange(sumUAH, currencyValues, exchangeCurrency) {
-    for (const item of currencyValues) {
-        if (item.currency === exchangeCurrency) {
-            return sumUAH / item.value;
-        }
-    }
-}
-
-const result = exchange(
-    10000,
-    [{currency: 'USD', value: 40}, {currency: 'EUR', value: 42}],
-    'USD'
-);
 console.log(result);
